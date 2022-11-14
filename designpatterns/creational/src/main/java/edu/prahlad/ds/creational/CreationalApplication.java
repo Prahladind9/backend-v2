69975fb83bd94ae9ff48eb403e33915d9707183a
Prahlad Rao <prahladskingdom@hotmail.com>
2022-11-14 13:38:08 +0100

    DP-C: Initial Commit

---
+++ designpatterns/creational/src/main/java/edu/prahlad/ds/creational/CreationalApplication.java
@@ -0,0 +1,13 @@
+package edu.prahlad.ds.creational;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreationalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreationalApplication.class, args);
+    }
+
+}
---
+++ designpatterns/creational/src/test/java/edu/prahlad/ds/creational/CreationalApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.prahlad.ds.creational;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreationalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
