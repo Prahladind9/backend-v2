f39cc4f68a893e88dfaca958bcb646b7ca80ed19
Prahlad Rao <prahladskingdom@hotmail.com>
2022-12-25 16:24:05 +0100

    DS-C: setup

---
+++ prep/interviewpatterns/src/main/java/edu/prahlad/interviewpatterns/InterviewpatternsApplication.java
@@ -0,0 +1,13 @@
+package edu.prahlad.interviewpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewpatternsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewpatternsApplication.class, args);
+    }
+
+}
---
+++ prep/interviewpatterns/src/test/java/edu/prahlad/interviewpatterns/InterviewpatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.prahlad.interviewpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewpatternsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
