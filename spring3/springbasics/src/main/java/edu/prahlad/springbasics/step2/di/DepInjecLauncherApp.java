eb32d98a66e0cffaf6bbce3b4baea9100d0bdc21
Prahlad Rao <prahladskingdom@hotmail.com>
2023-02-11 19:34:55 +0100

    SP: Constructor DI

--- spring3/springbasics/src/main/java/edu/prahlad/springbasics/step2/di/DepInjecLauncherApp.java
+++ spring3/springbasics/src/main/java/edu/prahlad/springbasics/step2/di/DepInjecLauncherApp.java
@@ -15,14 +15,9 @@ class YourBusinessClass{
     Dependency2 dependency2;
 
     @Autowired
-    public void setDependency1(Dependency1 dependency1) {
-        System.out.println("Setter Injection 1");
+    public YourBusinessClass(Dependency1 dependency1, Dependency2 dependency2) {
+        System.out.println("Constructor Injection");
         this.dependency1 = dependency1;
-    }
-
-    @Autowired
-    public void setDependency2(Dependency2 dependency2) {
-        System.out.println("Setter Injection 2");
         this.dependency2 = dependency2;
     }
 
