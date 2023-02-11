124a2a26241908829ebe4abd6da365411fb5c008
Prahlad Rao <prahladskingdom@hotmail.com>
2023-02-11 19:32:09 +0100

    SP: Setter Dependency Injection

---
+++ spring3/springbasics/src/main/java/edu/prahlad/springbasics/step2/di/DepInjecLauncherApp.java
@@ -0,0 +1,48 @@
+package edu.prahlad.springbasics.step2.di;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+class YourBusinessClass{
+    @Autowired
+    Dependency1 dependency1;
+    @Autowired
+    Dependency2 dependency2;
+
+    public void print(){
+        System.out.println("dependency1: "+ dependency1 + "dependency2: "+ dependency2);
+    }
+}
+
+@Component
+class Dependency1{
+
+}
+
+@Component
+class Dependency2{
+
+}
+
+
+@Configuration
+@ComponentScan
+public class DepInjecLauncherApp {
+    public static void main(String[] args) {
+        try(var context = new AnnotationConfigApplicationContext(DepInjecLauncherApp.class)){
+
+            Arrays.stream(context.getBeanDefinitionNames())
+                    .forEach(System.out::println);
+
+            System.out.println(context.getBean(YourBusinessClass.class));
+            context.getBean(YourBusinessClass.class).print();
+        }
+    }
+}
